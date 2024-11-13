c52234c32cd4977b209703d3b8669828049cce99
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-11-13 11:26:28 +0100

    add line for docker run

--- noxfile.py
+++ noxfile.py
@@ -1,3 +1,8 @@
+"""
+Can be run in container with
+ docker run --rm -it --user $(id -u):$(id -g) --mount type=bind,source=$(pwd),target=$(pwd) --workdir $(pwd) --platform linux/amd64 --entrypoint bash mambaorg/micromamba:debian12
+"""
+
 import nox
 
 
